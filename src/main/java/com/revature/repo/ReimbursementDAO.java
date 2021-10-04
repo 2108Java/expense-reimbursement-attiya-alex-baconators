11274736063dab83e4ab4409b1da3a527468c8aa
akoven <89858837+akoven@users.noreply.github.com>
2021-10-04 09:24:25 -0500

    Delete ReimbursementDAO.java

--- src/main/java/com/revature/repo/ReimbursementDAO.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repo;
-
-public interface ReimbursementDAO {
-
-	public boolean submitRequest();
-	public boolean checkRequestStatus();
-	public boolean editRequest();
-	public boolean removeRequest();
-}
