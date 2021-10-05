40a3c61eaebb800b89affc603b18aaed281f2767
akoven <attiya.kovenburg@revature.net>
2021-10-05 09:25:16 -0500

    adding new employee and manager registration table

---
+++ src/main/resources/Employee_manager registration table.sql
@@ -0,0 +1,2 @@
+create employee_login(employee_id serial primary key,firstname_emp varchar(20),lastname_emp varchar(30),address_emp varchar(50),city_emp varchar(20),state_emp varchar(2),zip_emp int, username_emp varchar(20), password_emp varchar(30));
+create manager_login(manager_id serial primary key, firstname_mgr varchar (20), lastname_mgr varchar(30), address_emp varchar(50),city_emp varchar(20),state_emp varchar(2),zip_emp int, username_mgr varchar(20), password_mgr varchar(30));
