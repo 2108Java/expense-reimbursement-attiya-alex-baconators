f4a4fbea30c1106180c665193cbaee2b1a7eac8c
akoven <attiya.kovenburg@revature.net>
2021-10-18 02:20:11 -0500

    changed Request.js and RequestForManager.js

--- src/main/resources/public/Request.js
+++ src/main/resources/public/Request.js
@@ -1,2 +1,31 @@
-let getRequest = document.getElementById("getRequest");
-getRequest.addEventListener('click', );
+let addRequestListener = document.getElementById("add");
+
+addRequestListener.addEventListener('click', addRedirect);
+
+function addRedirect(){
+    window.location.replace("http://localhost:9000/newExpense.html");
+}
+
+let removeRequestListener = document.getElementById("remove");
+
+removeRequestListener.addEventListener('click', removeRedirect);
+
+function removeRedirect(){
+    window.location.replace("http://localhost:9000/removeRequest.html");
+}
+
+let getRequestListener = document.getElementById("getRequest");
+
+getRequestListener.addEventListener('click', getRedirect);
+
+function getRedirect(){
+    window.location.replace("http://localhost:9000/getRequest.html");
+}
+
+let logoutRequestListener = document.getElementById("logout");
+
+logoutRequestListener.addEventListener('click', logoutRedirect);
+
+function logoutRedirect(){
+    window.location.replace("http://localhost:9000/preLogin.html");
+}
